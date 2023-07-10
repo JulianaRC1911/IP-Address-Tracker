27bb72122565884a03bdb933d456296d4c758085
Juliana Rizo <julianarizo1911@gmail.com>
2023-07-10 11:10:22 -0500

    Agregando archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'IP-Address-Tracker';
+  private map?: L.Map;
+  private marker?: L.Marker;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    window.onload = () => {
+      this.initMap();
+      this.getUserLocation();
+      this.getIPAddress();
+    };
+  }
+
+  initMap(): void {
+    this.map = L.map('map').setView([0, 0], 2);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(this.map);
+  }
+
+  getUserLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const { latitude, longitude } = position.coords;
+          this.addMarker(latitude, longitude);
+          this.map?.setView([latitude, longitude], 13); 
+        },
+        (error: GeolocationPositionError) => {
+          console.log(error);
+        }
+      );
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  }
+
+  addMarker(latitude: number, longitude: number): void {
+    const icon = L.icon({
+      iconUrl: '../assets/icon-location.svg',
+      iconSize: [32, 32],
+      iconAnchor: [16, 32]
+    });
+
+    this.marker = L.marker([latitude, longitude], { icon }).addTo(this.map!);
+  }
+
+  getIPAddress(): void {
+    this.http.get('https://api.ipify.org/?format=json').subscribe(
+      (response: any) => {
+        const ipAddress = response.ip;
+        this.searchAddress(ipAddress);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  searchAddress(inputValue: string): void {
+    const address = inputValue.trim();
+    if (address) {
+      if (this.isValidIPAddress(address)) {
+        this.searchIPAddress(address);
+      } else {
+        this.searchDomain(address);
+      }
+    }
+  }
+
+  isValidIPAddress(address: string): boolean {
+    const ipPattern = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+    return ipPattern.test(address);
+  }
+
+  searchIPAddress(ipAddress: string): void {
+    const apiKey = 'at_EuQH84lR2kOg0vEtZt31iAsdDDVXD';
+    if (this.marker) {
+      this.map?.removeLayer(this.marker);
+    }
+
+    this.http.get<any>(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ipAddress}`)
+      .subscribe(data => {
+        const { lat, lng } = data.location;
+        this.addMarker(lat, lng);
+        this.map?.setView([lat, lng], 13); 
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  searchDomain(domain: string): void {
+    const apiKey = 'at_EuQH84lR2kOg0vEtZt31iAsdDDVXD';
+
+    if (this.marker) {
+      this.map?.removeLayer(this.marker);
+    }
+
+    this.http.get<any>(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&domain=${domain}`)
+      .subscribe(data => {
+        const { lat, lng } = data.location;
+        this.addMarker(lat, lng);
+        this.map?.setView([lat, lng], 13); 
+      }, error => {
+        console.log(error);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
